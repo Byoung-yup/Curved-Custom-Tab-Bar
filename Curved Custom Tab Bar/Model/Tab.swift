3c4c69b4726b2af17a4734d521e4afbff42c00b3
ByoungYup <20170926@vision.hoseo.edu>
2024-01-16 01:21:12 +0900

    commit

--- Curved Custom Tab Bar/Model/Tab.swift
+++ Curved Custom Tab Bar/Model/Tab.swift
@@ -5,4 +5,28 @@
 //  Created by 김병엽 on 2024/01/16.
 //
 
-import Foundation
+import SwiftUI
+
+enum Tab: String, CaseIterable {
+    case home = "Home"
+    case services = "Services"
+    case partners = "Partners"
+    case activity = "Activity"
+    
+    var systemImage: String {
+        switch self {
+        case .home:
+            return "house"
+        case .services:
+            return "envelope.open.badge.clock"
+        case .partners:
+            return "hand.raised"
+        case .activity:
+            return "bell"
+        }
+    }
+    
+    var index: Int {
+        return Tab.allCases.firstIndex(of: self) ?? 0
+    }
+}
