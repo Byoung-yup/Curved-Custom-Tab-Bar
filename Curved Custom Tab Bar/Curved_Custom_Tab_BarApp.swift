4878f2f6c55a6ea3c33a8c66e43f4b431b8c3d34
ByoungYup <20170926@vision.hoseo.edu>
2024-01-16 00:12:07 +0900

    Initial Commit

---
+++ Curved Custom Tab Bar/Curved_Custom_Tab_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curved_Custom_Tab_BarApp.swift
+//  Curved Custom Tab Bar
+//
+//  Created by 김병엽 on 2024/01/16.
+//
+
+import SwiftUI
+
+@main
+struct Curved_Custom_Tab_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
